2e7e12b3ec189d04cc56e00022634855fe273284
christian162 <christian.sheridan151@gmail.com>
2021-10-15 21:41:33 -0500

    :zap: Start writing the inventory dsl

--- src/main/kotlin/me/codyq/spikot/gui/GUI.kt
+++ src/main/kotlin/me/codyq/spikot/gui/GUI.kt
@@ -1,41 +1,47 @@
 package me.codyq.spikot.gui
 
+import me.codyq.spikot.dsl.item
+import me.codyq.spikot.dsl.name
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+
 // TODO: Components, store logic for individual slot
 // TODO: Views for handling logic of rendering things to a GUI
 
-/*
+class GUI {
+    var view: View = View()
 
-fun myComponent(): Component = {
-    //
-}
+    fun showTo(player: Player) {
 
-var clicked = false
-
-val myView = view {
-    +component {
-        location = (x, y)
-        item = item(Material.WHITE_STAINED_GLASS)
-        onClick = {
-            // Toggle clicked variable
-            clicked = !clicked
-        }
     }
 
-    // Will only render if click is true
-    if (clicked) {
-        +myComponent {
-            location = (x, y)
-            item = item(Material.GREEN_STAINED_GLASS)
-        }
+    fun handleClick(player: Player, itemStack: ItemStack, slot: Pair<Int, Int>) {
+        val components = view.getComponents()
+        components.firstOrNull { it.location == slot }?.onClick?.let { it(player, itemStack) }
     }
 }
 
-val myGui = gui {
-    view = myView
+class View {
+    private val components = mutableListOf<GUIComponent>()
 
-    autoRefresh {
-        ticks = 1
-        onClick = true
+    fun component(body: GUIComponent.() -> Unit) {
+        val component = GUIComponent().apply(body)
+        components.add(component)
+    }
+
+    fun getComponents() = components.toList()
+}
+
+class GUIComponent {
+    var location: Pair<Int, Int> = Pair(0, 0)
+    var item: ItemStack = ItemStack(Material.AIR)
+    var onClick: (Player, ItemStack) -> Boolean = { player, itemStack -> false }
+        private set
+
+    fun onClick(body: (Player, ItemStack) -> Boolean) {
+        onClick = body
     }
 }
- */
+
+fun view(body: View.() -> Unit) = View().apply(body)
