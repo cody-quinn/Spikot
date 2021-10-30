bfae5badf7bc0fa695a04d34623e893a9e240628
christian162 <christian.sheridan151@gmail.com>
2021-10-30 06:48:58 -0500

    Add menu functionality

--- src/main/kotlin/me/codyq/spikot/gui/GUI.kt
+++ src/main/kotlin/me/codyq/spikot/gui/GUI.kt
@@ -1,28 +1,71 @@
 package me.codyq.spikot.gui
 
 import me.codyq.spikot.dsl.item
-import me.codyq.spikot.dsl.name
+import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.entity.Player
+import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.InventoryHolder
 import org.bukkit.inventory.ItemStack
+import kotlin.math.min
 
 // TODO: Components, store logic for individual slot
 // TODO: Views for handling logic of rendering things to a GUI
+// TODO: Add support for paginated guis.
 
-class GUI {
-    var view: View = View()
 
-    fun showTo(player: Player) {
+// val slot = (y * 9) + x
+class GUI: InventoryHolder {
+    var title: String = ""
 
+    private val views = mutableListOf<View>()
+
+    fun view(body: View.() -> Unit) {
+        val view = View().apply(body)
+        views.add(view)
     }
 
-    fun handleClick(player: Player, itemStack: ItemStack, slot: Pair<Int, Int>) {
-        val components = view.getComponents()
-        components.firstOrNull { it.location == slot }?.onClick?.let { it(player, itemStack) }
+    internal fun handleClick(player: Player, itemStack: ItemStack, slotX: Int, slotY: Int) {
+        views.forEach { view ->
+            val components = view.getComponents()
+            val firstComponent = components.firstOrNull { guiComponent ->  guiComponent.slotX == slotX && guiComponent.slotY == slotY }
+
+            if (firstComponent != null) {
+                firstComponent.onClick(player, itemStack)
+                return@forEach
+            }
+        }
+    }
+
+    override fun getInventory(): Inventory {
+        val inventoryHeight = views.maxByOrNull { it.height }?.height ?: 6
+        val createInventory = Bukkit.createInventory(this, min(54, inventoryHeight * 9), title)
+
+        var currentSlotX = 0
+
+        for (view in views) {
+            val height = view.height
+            val width = view.width
+
+            
+        }
+
+        return createInventory
     }
 }
 
 class View {
+    private var _width: Int = 0
+    private var _height: Int = 0
+
+    var width: Int
+        get() = _width
+        set(value) { _width = min(9, value) }
+
+    var height: Int
+        get() = _height
+        set(value) { _height = min(6, value) }
+
     private val components = mutableListOf<GUIComponent>()
 
     fun component(body: GUIComponent.() -> Unit) {
@@ -33,10 +76,14 @@ class View {
     fun getComponents() = components.toList()
 }
 
+// TODO: slots should be relative to the view.
 class GUIComponent {
-    var location: Pair<Int, Int> = Pair(0, 0)
+    var slotX: Int = 0
+    var slotY: Int = 0
+
     var item: ItemStack = ItemStack(Material.AIR)
-    var onClick: (Player, ItemStack) -> Boolean = { player, itemStack -> false }
+
+    var onClick: (Player, ItemStack) -> Boolean = { _, _ -> false }
         private set
 
     fun onClick(body: (Player, ItemStack) -> Boolean) {
@@ -44,4 +91,4 @@ class GUIComponent {
     }
 }
 
-fun view(body: View.() -> Unit) = View().apply(body)
+fun gui(body: GUI.() -> Unit) = GUI().apply(body)
